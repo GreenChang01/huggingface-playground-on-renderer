a4cbd8bce06aa84e06a7532f6ebb5020009c2cd6
jun.zhang10 <jun.zhang10@weimob.com>
2024-03-02 20:11:31 +0800

    feat: modify @gradio/client

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const express = require("express");
+import { createRequire } from "node:module";
+import { client } from "./client.js";
+import express from "express";
+const require = createRequire(import.meta.url);
 const app = express();
+global.EventSource = require("eventsource");
+const baseUrl =
+  "https://playgroundai-playground-v2-5.hf.space/--replicas/9kuov/";
+const huggingFaceApp = await client(baseUrl);
+
 const port = process.env.PORT || 3001;
-const importDynamic = new Function('modulePath', 'return import(modulePath)');
 
 const handleRunGenerateImage = async (req, res) => {
-  const { client } = await importDynamic('@gradio/client');
-  const huggingFaceApp = await client("https://playgroundai-playground-v2-5.hf.space/--replicas/9kuov/");
-  const { posPrompt,negPrompt,useNeg,width,height, guidanceScale } = req.body
-  const result = await huggingFaceApp.predict("/run", [		
-    posPrompt || "Hello!!", // string  in 'Prompt' Textbox component		
-    negPrompt || "Hello!!", // string  in 'Negative prompt' Textbox component		
-    useNeg || true, // boolean  in 'Use negative prompt' Checkbox component		
-    0, // number (numeric value between 0 and 2147483647) in 'Seed' Slider component		
-    width || 256, // number (numeric value between 256 and 1536) in 'Width' Slider component		
-    height || 256, // number (numeric value between 256 and 1536) in 'Height' Slider component		
-    guidanceScale || 0.1, // number (numeric value between 0.1 and 20) in 'Guidance Scale' Slider component		
+  console.log("req: ", req);
+  const { posPrompt, negPrompt, useNeg, width, height, guidanceScale } =
+    req.body;
+  const result = await huggingFaceApp.predict("/run", [
+    posPrompt || "Hello!!", // string  in 'Prompt' Textbox component
+    negPrompt || "", // string  in 'Negative prompt' Textbox component
+    useNeg || false, // boolean  in 'Use negative prompt' Checkbox component
+    0, // number (numeric value between 0 and 2147483647) in 'Seed' Slider component
+    width || 1024, // number (numeric value between 1024 and 1536) in 'Width' Slider component
+    height || 1024, // number (numeric value between 1024 and 1536) in 'Height' Slider component
+    guidanceScale || 3, // number (numeric value between 0.1 and 20) in 'Guidance Scale' Slider component
     true, // boolean  in 'Randomize seed' Checkbox component
   ]);
-  console.log(result.data);
-}
-
+  if (result?.data?.[0]?.[0]?.image?.path) {
+    const fileUrl = `${baseUrl}file=${result.data[0][0].image.path}`;
+    console.log(fileUrl);
+    res.send(fileUrl);
+  }
+};
 
 app.post("/", handleRunGenerateImage);
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-
